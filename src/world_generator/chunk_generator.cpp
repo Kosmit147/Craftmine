71ea0640ce4272bc7e7a591138592fe2efcb5d3a
Wojciech Opara <wwojtek660@gmail.com>
2025-04-04 22:56:34 +0200

    Update zenith

--- src/world_generator/chunk_generator.cpp
+++ src/world_generator/chunk_generator.cpp
@@ -1,33 +1,32 @@
 #include "chunk_generator.hpp"
-#include <glm/glm.hpp>
 
 ChunkGenerator::ChunkGenerator(int seed) : seed(seed) {}
 
 float ChunkGenerator::get_height(float worldX, float worldZ) const
 {
-    glm::vec2 pos(worldX, worldZ);
-    float noiseValue = zth::Random::perlin_noise<2, float>(pos);
+    glm::vec2 pos{ worldX, worldZ };
+    float noiseValue = zth::Random::perlin_noise(pos);
     return noiseValue * 0.5f + 0.5f;
 }
 
 ChunkData ChunkGenerator::generate_chunk(int chunkX, int chunkZ)
 {
-    ChunkData chunk(CHUNK_WIDTH, std::vector<std::vector<BlockType>>(CHUNK_DEPTH, std::vector<BlockType>(CHUNK_HEIGHT, BlockType::Air)));
+    ChunkData chunk(chunk_width, std::vector(chunk_depth, std::vector(chunk_height, BlockType::Air)));
 
     float scale = 0.05f;
     int baseHeight = 64;
 
-    for (int x = 0; x < CHUNK_WIDTH; ++x)
+    for (int x = 0; x < chunk_width; ++x)
     {
-        for (int z = 0; z < CHUNK_DEPTH; ++z)
+        for (int z = 0; z < chunk_depth; ++z)
         {
-            float worldX = (chunkX * CHUNK_WIDTH + x) * scale;
-            float worldZ = (chunkZ * CHUNK_DEPTH + z) * scale;
+            float worldX = (chunkX * chunk_width + x) * scale;
+            float worldZ = (chunkZ * chunk_depth + z) * scale;
 
             float noise = get_height(worldX, worldZ);
             int height = baseHeight + static_cast<int>(noise * 30.0f);
 
-            for (int y = 0; y <= height && y < CHUNK_HEIGHT; ++y)
+            for (int y = 0; y <= height && y < chunk_height; ++y)
             {
                 if (y == height)
                     chunk[x][z][y] = BlockType::Grass;
