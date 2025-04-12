451a842d593584d89e3cfeb23b58e7da057fbcbc
Wojciech Opara <wwojtek660@gmail.com>
2025-04-12 18:16:36 +0200

    Refactor

--- src/main_scene.hpp
+++ src/main_scene.hpp
@@ -1,20 +1,24 @@
 #pragma once
 
 #include "world/chunk.hpp"
-#include <assets.hpp>
-inline zth::gl::TextureParams texture_params = {.mag_filter = zth::gl::TextureMagFilter::nearest};
+
 class MainScene : public zth::Scene
 {
 public:
-    explicit MainScene() = default;
+    constexpr static glm::vec4 sky_color{ 110.0f / 255.0f, 177.0f / 255.0f, 1.0f, 1.0f };
+
+public:
+    explicit MainScene();
     ZTH_NO_COPY_NO_MOVE(MainScene)
     ~MainScene() override = default;
 
 private:
     zth::EntityHandle _camera = create_entity("Camera");
     zth::EntityHandle _directional_light = create_entity("Directional Light");
-    zth::gl::Texture2D _blocks_texture{ blocks_texture_data, texture_params };
-    zth::Material _block_material{ .diffuse_map = &_blocks_texture };
+
+    zth::gl::Texture2D _blocks_texture;
+    zth::Material _chunk_material;
+
     zth::Vector<Chunk> _chunks;
 
 private:
